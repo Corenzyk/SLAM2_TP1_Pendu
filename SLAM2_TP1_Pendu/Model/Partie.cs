d71467d68893791b05d8f2bf1d42651a516b146c
Corenzyk K <corentin.kuczyk49@gmail.com>
2024-02-09 08:54:24 +0100

    TP1 terminé, récupération réalisation apres changement de nom de projet annulé

--- SLAM2_TP1_Pendu/Model/Partie.cs
+++ SLAM2_TP1_Pendu/Model/Partie.cs
@@ -12,7 +12,7 @@ namespace SLAM2_TP1_Pendu.Model
         public Int32 nbEssais;
         public String motatrouver;
         public String motaafficher;
-        public List<String> listeATrouver;
+
 
         public void changerIMG(PictureBox pb_pendu)
         {
@@ -58,7 +58,7 @@ namespace SLAM2_TP1_Pendu.Model
             motaafficher = "";
             bool erreur = false;
 
-            for (int i = 0; i<cArray.Count(); i++)
+            for (int i = 0; i < cArray.Count(); i++)
             {
                 if (cArray[i] == Convert.ToChar(lettretape))
                 {
@@ -79,7 +79,7 @@ namespace SLAM2_TP1_Pendu.Model
         {
             string motaff = "";
 
-            for (int i=0;i<motatrouver.Count();i++)
+            for (int i = 0; i < motatrouver.Count(); i++)
                 motaff += "_";
 
             motaafficher = motaff;
@@ -92,5 +92,54 @@ namespace SLAM2_TP1_Pendu.Model
             motatrouver = listeATrouver[nbAleatoire].ToUpper();
         }
 
+        public void victoire(Form formulaireJeuActif, TextBox txt_motAafficher, List<string> listeMotaTrouver, PictureBox pbpendu)
+        {
+            DialogResult msg;
+            if (nbEssais >= 8)
+            {
+                msg = MessageBox.Show("Vous avez perdu !! \r\nVous deviez trouver le mot: " + motatrouver + "\r\nVoulez vous faire une autre partie ??", "You loose", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation, MessageBoxDefaultButton.Button1);
+                if (msg == DialogResult.Yes)
+                {
+                    remiseAZero(formulaireJeuActif, txt_motAafficher, listeMotaTrouver, pbpendu);
+                }
+                else if (msg == DialogResult.No)
+                {
+                    Form1 Accueil = new Form1();
+                    Accueil.Show();
+                    formulaireJeuActif.Hide();
+                }
+            }
+            else if (motatrouver == motaafficher)
+            {
+                msg = MessageBox.Show("Vous avez gagné !! \r\nVous avez trouvé le mot: " + motatrouver + "\r\nVoulez vous faire une autre partie ??", "You win", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation, MessageBoxDefaultButton.Button1);
+                if (msg == DialogResult.Yes)
+                {
+                    remiseAZero(formulaireJeuActif, txt_motAafficher, listeMotaTrouver, pbpendu);
+                }
+                else if (msg == DialogResult.No)
+                {
+                    Form1 Accueil = new Form1();
+                    Accueil.Show();
+                    formulaireJeuActif.Hide();
+                }
+            }
+        }
+
+        public void remiseAZero(Form formulaireJeuActif, TextBox txt_motAafficher, List<String> listeMotaTrouver, PictureBox pb_pendu)
+        {
+            motaafficher = "";
+            motatrouver = "";
+            choisirMotATrouver(listeMotaTrouver);
+            genererMotAfficher(motatrouver);
+            txt_motAafficher.Text = motaafficher;
+
+            foreach (Control c in formulaireJeuActif.Controls)
+            {
+                if (c.GetType() == typeof(Button))
+                    c.Enabled = true;
+            }
+            nbEssais = 0;
+            changerIMG(pb_pendu);
+        }
     }
 }
