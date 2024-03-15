906bcb555e67ca322a9e2e7fb14ad511e8dcc06f
Corenzyk K <corentin.kuczyk49@gmail.com>
2024-03-15 09:40:22 +0100

    Création de la classe Mots dans le dossier Controllers

---
+++ SLAM2_TP1_Pendu/Controllers/Mots.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SLAM2_TP1_Pendu.Controllers
+{
+    internal class Mots
+    {
+    }
+}
