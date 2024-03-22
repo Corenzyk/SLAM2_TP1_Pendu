8df3132324482462c134c6c214ffc28d741cdc19
Corenzyk K <corentin.kuczyk49@gmail.com>
2024-03-22 10:02:10 +0100

    Donnees.cs finie (PHASE 5 en cours)

--- SLAM2_TP1_Pendu/Donnees.cs
+++ SLAM2_TP1_Pendu/Donnees.cs
@@ -7,15 +7,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using SLAM2_TP1_Pendu.Controllers;
 using SLAM2_TP1_Pendu.Model;
 
 namespace SLAM2_TP1_Pendu
 {
     public partial class Donnees : Form
     {
+        private DataView dv;
+        private DataGridView dgv_Mots;
+        private Mots dt_mot;
         public Donnees()
         {
             InitializeComponent();
+            dt_mot = new Mots();
+            dv = new DataView(dt_mot.GetListeMots());
+            dgv_Mots.DataSource = dv;
+            dgv_Mots.Columns["IDMOTS"].Visible = false;
+            dgv_Mots.Columns["Mots"].Width = 190;
+
         }
 
         private void btn_close_Click_1(object sender, EventArgs e)
